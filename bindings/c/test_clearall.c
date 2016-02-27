5d4256046e77e436d2eb9ce5cf3c6fabd9977568
Richard Townsend <richard@sentimentron.co.uk>
2016-02-27 18:30:18 +0000

    CLEARALL tests

---
+++ bindings/c/test_clearall.c
@@ -0,0 +1,15 @@
+#include <assert.h>
+#include <string.h>
+#include "dst_internal.h"
+
+/* The PUCK command is used to check if a server
+   is accessible */
+
+int main(int argc, char **argv) {
+
+  char buf[1024];
+  assert(dst_trans_fixed_out("CLEARALL", buf) == 0);
+  assert(strncmp(buf, "OK", 2) == 0);
+
+  return 0;
+}
